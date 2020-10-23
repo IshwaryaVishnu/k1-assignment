cd48800375fa219d21cf9fcc7f48d6038342ae22
IshwaryaVishnu <ishu27.v@gmail.com>
2020-10-23 17:06:24 +0530

    project4

--- assignment4.py
+++ assignment4.py
@@ -1,5 +1,5 @@
 import json
-para =  '{ "Name":"Iswarya", "Code":"9", "Age":28 }'
+para ='{ "Name":"Iswarya", "Code":"9", "Age":28 }'
 Emp_details = json.loads(para)
 print("\nJSON data:")
 print(Emp_details)
@@ -7,8 +7,9 @@ print("\nName: ",Emp_details["Name"])
 print("Class: ",Emp_details["Code"])
 print("Age: ",Emp_details["Age"])
 
-
-
+blog = {'URL': 'Kloudone.com', 'name': 'Kloudone'}
+to_json = json.dumps(blog)
+print(to_json)
 
 
 
