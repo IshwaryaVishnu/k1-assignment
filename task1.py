b9365ee4909d674f7dc248bcc56217e2902b499d
IshwaryaVishnu <ishu27.v@gmail.com>
2020-10-30 11:08:46 +0530

    project4

---
+++ task1.py
@@ -0,0 +1,18 @@
+import json
+keys =["Iswarya", "Anjali","Swetha","Sweety"]
+values =["python", "Java","salesforce"]
+data = dict(zip(keys,values))
+print(data)
+print("\nJSON data:")
+print(json.dumps(data, sort_keys=True, indent=4))
+
+
+
+
+
+
+
+
+
+
+
