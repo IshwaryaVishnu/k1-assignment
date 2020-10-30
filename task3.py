e55b5f3882abc6c16c4b14eca6a964fcf6242d3a
IshwaryaVishnu <ishu27.v@gmail.com>
2020-10-30 16:46:27 +0530

    assign3

---
+++ task3.py
@@ -0,0 +1,13 @@
+import os
+cwd = os.getcwd()
+print("Current Directory:", cwd)
+script = os.path.realpath(__file__)
+print("Script path:", script)
+print(os.listdir())
+try:
+   file_Name = 'task3.py'
+   f = open(file_Name, 'r')
+   text = f.read()
+   f.close()
+except IOError:
+   print('Not accessed or problem in reading: ' + file_Name)
